ea284d98a350af7a6f04177a93c32a9c6ffd30e5
fpatel3 <forampatel3010@gmail.com>
2021-08-18 00:00:10 -0400

    MAde changes

--- CP380-B3-BlockBlazor/Data/BlockService.cs
+++ CP380-B3-BlockBlazor/Data/BlockService.cs
@@ -14,16 +14,40 @@ namespace CP380_B3_BlockBlazor.Data
         //       - httpClient
         //       - configuration
         //
+        static HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+       
 
         //
         // TODO: Add a constructor with IConfiguration and IHttpClientFactory arguments
         //
 
+         public BlockService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _config = configuration.GetSection("BlockService");
+        }
+
         //
         // TODO: Add an async method that returns an IEnumerable<Block> (list of Blocks)
         //       from the web service
         //
+         public async Task<IEnumerable<Block>> GetBlock()
+        {
+            var responce = await _httpClient.GetAsync(_config["url"]);
 
+            if (responce.IsSuccessStatusCode)
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+                return await JsonSerializer.DeserializeAsync<IEnumerable<Block>>(
+                        await responce.Content.ReadAsStreamAsync() , options
+                    );
+            }
+
+            return Array.Empty<Block>();
+        }
+
+    
     }
-}
 }
--- CP380-B3-BlockBlazor/Data/PendingTransactionService.cs
+++ CP380-B3-BlockBlazor/Data/PendingTransactionService.cs
@@ -16,14 +16,38 @@ namespace CP380_B3_BlockBlazor.Data
         //       - configuration
         //
 
+        static HttpClient _httpClient;
+        private IConfiguration _config { get; set; }
+
         //
         // TODO: Add a constructor with IConfiguration and IHttpClientFactory arguments
-        //
+        
+
+         public PendingTransactionService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _config = configuration.GetSection("PayloadService");
+        }
 
         //
         // TODO: Add an async method that returns an IEnumerable<Payload> (list of Payloads)
         //       from the web service
         //
+
+         public async Task<IEnumerable<Payload>> GetPayloads()
+        {
+            var responce = await _httpClient.GetAsync(_config["url"]);
+
+            if (responce.IsSuccessStatusCode)
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+                return await JsonSerializer.DeserializeAsync<IEnumerable<Payload>>(
+                        await responce.Content.ReadAsStreamAsync(), options
+                    );
+            }
+
+            return Array.Empty<Payload>();
+        }
 
         //
         // TODO: Add an async method that returns an HttpResponseMessage
